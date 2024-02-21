89462245a09105ddf99760f54bef51d8ce861411
Mark Mwima <markmwimar@gmail.com>
2024-02-21 12:25:05 +0300

    app configuration

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const express = require('express');
+
+const db = require('./data/database');
+const mainRoutes = require('./routes/main.routes');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+
+app.use(mainRoutes);
+
+app.use(function (error, req, res, next) {
+    res.status(500).render('500');
+})
+
+db.initDatabase().then(function () {
+    app.listen(3000);
+}).catch(function (error) {
+    console.log('Connecting to database failed!')
+});
