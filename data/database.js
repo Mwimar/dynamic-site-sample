81b20300e6e725267f99c127b23a336a2318ac24
Mark Mwima <markmwimar@gmail.com>
2024-02-21 13:12:55 +0300

    database config

---
+++ data/database.js
@@ -0,0 +1,22 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+let database;
+
+async function initDatabase() {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    database = client.db('deployment');
+}
+
+function getDb() {
+    if (!database) {
+        throw new Error('No DataBase connected');
+    }
+    return database;
+}
+
+module.exports = {
+    initDatabase: initDatabase,
+    getDb: getDb,
+};
